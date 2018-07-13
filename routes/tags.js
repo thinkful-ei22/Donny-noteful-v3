1d28d3087996bed2df9b9550a7fc69ca7179af72
Jonny Cheng <jcheng02@mica.edu>
2018-07-13 12:20:25 -0400

    POST tag route

--- routes/tags.js
+++ routes/tags.js
@@ -31,6 +31,57 @@ router.get('/', (req, res, next) => {
 /*Get me Tag Boi*/
 
 
-router.get('/:id')
+router.get('/:id', (req,res, next) => {
+  const {id} = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  Tag.findById(id)
+    .then(result => {
+      if(result){
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
+
+});
+
+
+//Tag - The Creation!
+
+router.post('/', (req, res, next) => {
+  const {name} = req.body;
+  const newTag = { name };
+  
+  if (!newTag.name) {
+    const err = new Error('Missing `name` in the request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  Tag.create(newTag)
+    .then (result => {
+      if (result){
+        res.location(`${req.originalUrl}/${res.id}`).status(201).json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The folder name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+
+});
+
 
 module.exports = router;
